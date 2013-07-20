5374ac4b3fc03602cf111450fcb38d1e67c5ebd3
Shogo Kawahara <shogo.a.kawahara@mail.rakuten.com>
2013-07-20 17:31:18 +0900

    Add --ttl, --dry-run, --without-hashed-file and --hashed-file-ttl option to console

--- console.php
+++ console.php
@@ -18,11 +18,20 @@ $console = new Application();
 $console
     ->register('cache:clear-old-file')
     ->setDefinition(array(
-        new InputOption('ttl', 't', InputOption::VALUE_OPTIONAL, 'ttl', 300)
+        new InputOption('ttl', 't', InputOption::VALUE_OPTIONAL, 'ttl', 300), // 5 minutes
+        new InputOption('dry-run', null, InputOption::VALUE_NONE, ''),
+        new InputOption('without-hashed-file', null, InputOption::VALUE_NONE, ''),
+        new InputOption('hashed-file-ttl', null, InputOption::VALUE_OPTIONAL, '', 60*60*24*6) //6 months
     ))
     ->setDescription('Clear old cache files')
     ->setCode(function (InputInterface $input, OutputInterface $output) use ($app) {
         $ttl = $input->getOption('ttl');
+        $dryRun = $input->getOption('dry-run');
+        $dryRunText = '';
+        if ($dryRun) {
+            $dryRunText = ' (dry-run)';
+        }
+
         $finder = new Finder();
         $fs = new Filesystem();
         $finder->files()
@@ -30,21 +39,48 @@ $console
             ->date('until '.$ttl.' sec ago')
             ->in($app['cache_dir']);
         foreach ($finder as $file) {
-            $output->writeln('<info>Remove '.$file->getRealpath().'</info>');
-            $fs->remove($file);
+            $output->writeln('<info>Remove '.$file->getRealpath().$dryRunText.'</info>');
+            if (!$dryRun) {
+                $fs->remove($file);
+            }
+        }
+
+        if (!$input->getOption('without-hashed-file')) {
+            $hashedFileFinder = new Finder();
+            $hashedFileTtl = $input->getOption('hashed-file-ttl');
+            $hashedFileFinder->files()
+                ->name('/\$[0-9a-f]+\.json$/')
+                ->date('until '.$hashedFileTtl.' sec ago')
+                ->in($app['cache_dir']);
+            foreach ($hashedFileFinder as $file) {
+                $output->writeln('<info>Remove '.$file->getRealpath().$dryRunText.'</info>');
+                if (!$dryRun) {
+                    $fs->remove($file);
+                }
+            }
         }
     });
 
 $console
     ->register('cache:clear-all')
+    ->setDefinition(array(
+        new InputOption('dry-run', null, InputOption::VALUE_NONE, ''),
+    ))
     ->setDescription('Clear all cache files')
     ->setCode(function (InputInterface $input, OutputInterface $output) use ($app) {
+        $dryRun = $input->getOption('dry-run');
+        $dryRunText = '';
+        if ($dryRun) {
+            $dryRunText = ' (dry-run)';
+        }
         $finder = new Finder();
         $fs = new Filesystem();
         $finder->files()->in($app['cache_dir']);
         foreach ($finder as $file) {
-            $output->writeln('<info>Remove '.$file->getRealpath().'</info>');
-            $fs->remove($file);
+            $output->writeln('<info>Remove '.$file->getRealpath().$dryRunText.'</info>');
+            if (!$dryRun) {
+                $fs->remove($file);
+            }
         }
     });
 
